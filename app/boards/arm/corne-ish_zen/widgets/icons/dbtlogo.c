49694603948473a0a54072436278116a0bb8be59
aescay <andrew.escay@gmail.com>
2022-04-10 14:42:20 -0400

    align configs

--- app/boards/arm/corne-ish_zen/widgets/icons/dbtlogo.c
+++ app/boards/arm/corne-ish_zen/widgets/icons/dbtlogo.c
@@ -56,7 +56,6 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_DBTLOGO u
 const lv_img_dsc_t dbtlogo = {
   .header.cf = LV_IMG_CF_INDEXED_1BIT,
   .header.always_zero = 0,
-  .header.reserved = 0,
   .header.w = 72,
   .header.h = 38,
   .data_size = 350,
